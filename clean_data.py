ba33ec0bb5ec395b65a98a486391f838faf46175
Jacky Lee <jla524@sfu.ca>
2019-08-12 17:46:53 -0700

    Update data cleaning functions

--- clean_data.py
+++ clean_data.py
@@ -1,38 +1,39 @@
-import os, sys, itertools
+import sys, pickle, itertools
 import numpy as np
 import pandas as pd
-
-tables = ['circuits', 'constructor_results', 'constructor_standings', 'constructors', 'driver_standings', \
-		  'drivers', 'lap_times', 'pit_stops', 'qualifying', 'races', 'results', 'seasons', 'status']
-
-circuits_n = ['circuitId', 'circuitRef', 'name', 'location', 'country', 'lat', 'lng', 'alt', 'url']
-constr_re_n = ['constructorResultsId', 'raceId', 'constructorId', 'points', 'status']
-constr_stand_n = ['constructorStandingsId', 'raceId', 'constructorId', \
-				  'points', 'position', 'positionText', 'wins']
-constr_n = ['constructorId', 'constructorRef', 'name', 'nationality', 'url']
-driver_stand_n = ['driverStandingsId', 'raceId', 'driverId', 'points', 'position', 'positionText', 'wins']
-drivers_n = ['driverId', 'driverRef', 'number', 'code', 'forename', 'surname', 'dob', 'nationality', 'url']
-lap_times_n = ['raceId', 'driverId', 'lap', 'position', 'time', 'milliseconds']
-pit_stops_n = ['raceId', 'driverId', 'stop', 'lap', 'time', 'duration', 'milliseconds']
-qualifying_n = ['qualifyId', 'raceId', 'constructorId', 'number', 'position', 'q1', 'q2', 'q3']
-races_n = ['raceId', 'year', 'round', 'circuitId', 'name', 'date', 'time', 'url']
-results_n = ['resultId', 'raceId', 'driverId', 'constructorId', 'number', 'grid', 'position', \
-			 'positionText', 'positionOrder', 'points', 'laps', 'time', 'milliseconds', \
-			 'fastestLap', 'rank', 'fastestLapTime', 'fastestLapSpeed', 'statusId']
-seasons_n = ['year', 'url']
-status_n = ['statusId', 'status']
-
-names_list = [circuits_n, constr_re_n, constr_stand_n, constr_n, driver_stand_n, drivers_n, \
-		 lap_times_n, pit_stops_n, qualifying_n, races_n, results_n, seasons_n, status_n]
+from os import listdir, makedirs
+from os.path import isfile, isdir, join
+from create_columns import create_columns
 
 
-def main(input='f1db_csv/', output='clean_f1db/'):
-	assert len(tables) == len(names_list)
+def load_columns(path):
+	# Load the text files containing tables and column names
+	# Or create them if they do not exist
+	if not isfile('tables.txt') or not isfile('columns.txt'):
+		create_columns()
 
-	if not os.path.exists(output):
-		os.makedirs(output)
+	with open('tables.txt', 'rb') as file:
+		tables =  pickle.load(file)
 
-	for (table, names) in zip(tables, names_list):
+	with open('columns.txt', 'rb') as file:
+		column_names =  pickle.load(file)
+
+	# Make sure that number of files == number of lists in columns.txt
+	assert len(tables) == len(column_names)
+
+	return tables, column_names
+
+
+def clean_data(input='f1db_csv/', output='clean_f1db/'):
+	# Load file names and columns names
+	tables, column_names = load_columns(input)
+
+	# Create the output directory is it does not exist
+	if not isdir(output):
+		makedirs(output)
+
+	# For each table, add the column names and replace NaN values
+	for (table, names) in zip(tables, column_names):
 		filename = table + '.csv'
 		df = pd.read_csv(input + filename, header=None, names=names)
 		df = df.replace(r'\N', np.NaN)
@@ -43,10 +44,8 @@ if __name__ == '__main__':
 	num_inputs = len(sys.argv)
 
 	if (num_inputs == 1):
-		main()	
+		clean_data()	
 	elif (num_inputs == 3):
-		input = sys.argv[0]
-		output = sys.argv[1]
-		main(input, output)
+		clean_data(sys.argv[0], sys.argv[1])
 	else:
-		print("Expected zero or two inputs.")
+		print("Expected zero or two input arguments.")
---
+++ create_columns.py
@@ -0,0 +1,40 @@
+import pickle
+
+tables = ['circuits', 'constructor_results', 'constructor_standings', 'constructors', 'driver_standings', \
+		  'drivers', 'lap_times', 'pit_stops', 'qualifying', 'races', 'results', 'seasons', 'status']
+
+circuits_n = ['circuitId', 'circuitRef', 'name', 'location', 'country', 'lat', 'lng', 'alt', 'url']
+constr_re_n = ['constructorResultsId', 'raceId', 'constructorId', 'points', 'status']
+constr_stand_n = ['constructorStandingsId', 'raceId', 'constructorId', \
+				  'points', 'position', 'positionText', 'wins']
+constr_n = ['constructorId', 'constructorRef', 'name', 'nationality', 'url']
+driver_stand_n = ['driverStandingsId', 'raceId', 'driverId', 'points', 'position', 'positionText', 'wins']
+drivers_n = ['driverId', 'driverRef', 'number', 'code', 'forename', 'surname', 'dob', 'nationality', 'url']
+lap_times_n = ['raceId', 'driverId', 'lap', 'position', 'time', 'milliseconds']
+pit_stops_n = ['raceId', 'driverId', 'stop', 'lap', 'time', 'duration', 'milliseconds']
+qualifying_n = ['qualifyId', 'raceId', 'constructorId', 'number', 'position', 'q1', 'q2', 'q3']
+races_n = ['raceId', 'year', 'round', 'circuitId', 'name', 'date', 'time', 'url']
+results_n = ['resultId', 'raceId', 'driverId', 'constructorId', 'number', 'grid', 'position', \
+			 'positionText', 'positionOrder', 'points', 'laps', 'time', 'milliseconds', \
+			 'fastestLap', 'rank', 'fastestLapTime', 'fastestLapSpeed', 'statusId']
+seasons_n = ['year', 'url']
+status_n = ['statusId', 'status']
+
+column_names = [circuits_n, constr_re_n, constr_stand_n, constr_n, driver_stand_n, drivers_n, \
+		 lap_times_n, pit_stops_n, qualifying_n, races_n, results_n, seasons_n, status_n]
+
+
+def create_columns():
+	# Convert tables to a text file
+	with open('tables.txt', 'wb') as file:
+		pickle.dump(tables, file)
+
+	# Do the same with column names
+	with open('columns.txt', 'wb') as file:
+		pickle.dump(column_names, file)
+
+	print('Columns have been created.')
+
+
+if __name__ == '__main__':
+	create_columns()
