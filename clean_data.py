806f808daaba38e72e7ab5602cb0e05c26122303
Jacky Lee <39754370+jla524@users.noreply.github.com>
2021-02-22 00:19:14 -0800

    Improve code style in clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,4 +1,6 @@
-import sys, pickle, itertools
+import sys
+import pickle
+import itertools
 import numpy as np
 import pandas as pd
 from os import listdir, makedirs
@@ -9,8 +11,8 @@ def clean_data(input='f1db_csv', output='clean_f1db'):
   if not isdir(output):
     makedirs(output)
 
-  # For each table, add the column names and handling missing values
-  for filename in listdir("f1db_csv"):
+  # For each file, set column names and handle missing values
+  for filename in listdir('f1db_csv'):
     df = pd.read_csv(join(input, filename))
     df = df.replace(r'\N', np.NaN)
     df.to_csv(join(output, filename), index=False)
@@ -19,11 +21,11 @@ def clean_data(input='f1db_csv', output='clean_f1db'):
 if __name__ == '__main__':
   num_inputs = len(sys.argv)
 
-  if (num_inputs == 1):
+  if num_inputs == 1:
     clean_data()
     print("Cleaned data is now stored in clean_f1db/")
-  elif (num_inputs == 3):
+  elif num_inputs == 3:
     clean_data(sys.argv[1], sys.argv[2])
-    print("Cleaned data is now stored in " + sys.argv[2] + "/")
+    print("Cleaned data is now stored in " + sys.argv[2] + '/')
   else:
-    print("Expected zero or two input arguments.")
+    print("Expected either zero or two input arguments")
