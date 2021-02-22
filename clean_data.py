9b656c262b625cccc5595753aa2ee4c19df60ba5
Jacky Lee <jla524@sfu.ca>
2021-02-21 19:52:32 -0800

    Update clean_data.py to infer column names

--- clean_data.py
+++ clean_data.py
@@ -3,39 +3,15 @@ import numpy as np
 import pandas as pd
 from os import listdir, makedirs
 from os.path import isfile, isdir, join
-from create_columns import create_columns
-
-
-def load_columns(path):
-  # Load the text files containing tables and column names
-  # Or create them if they do not exist
-  if not isfile('tables.txt') or not isfile('columns.txt'):
-    create_columns()
-
-  with open('tables.txt', 'rb') as file:
-    tables =  pickle.load(file)
-
-  with open('columns.txt', 'rb') as file:
-    column_names =  pickle.load(file)
-
-  # Ensure that number of files == number of lists in columns.txt
-  assert len(tables) == len(column_names)
-
-  return tables, column_names
 
 
 def clean_data(input='f1db_csv', output='clean_f1db'):
-  # Load file names and columns names
-  tables, column_names = load_columns(input)
-
-  # Create the output directory is it does not exist
   if not isdir(output):
     makedirs(output)
 
-  # For each table, add the column names and replace NaN values
-  for (table, names) in zip(tables, column_names):
-    filename = table + '.csv'
-    df = pd.read_csv(join(input, filename), header=0, names=names)
+  # For each table, add the column names and handling missing values
+  for filename in listdir("f1db_csv"):
+    df = pd.read_csv(join(input, filename))
     df = df.replace(r'\N', np.NaN)
     df.to_csv(join(output, filename), index=False)
 
@@ -44,8 +20,10 @@ if __name__ == '__main__':
   num_inputs = len(sys.argv)
 
   if (num_inputs == 1):
-    clean_data()	
+    clean_data()
+    print("Cleaned data is now stored in clean_f1db/")
   elif (num_inputs == 3):
-    clean_data(sys.argv[0], sys.argv[1])
+    clean_data(sys.argv[1], sys.argv[2])
+    print("Cleaned data is now stored in " + sys.argv[2] + "/")
   else:
     print("Expected zero or two input arguments.")
