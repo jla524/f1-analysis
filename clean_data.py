1d099692e7e750202beb890c9408237f0f4dc10a
Jacky Lee <jla524@sfu.ca>
2021-05-09 23:01:15 -0700

    Refactor clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 import numpy as np
 import pandas as pd
@@ -5,25 +6,19 @@ from os import listdir, makedirs
 from os.path import isfile, isdir, join
 
 
-def clean_data(input='f1db_csv', output='clean_f1db'):
-  if not isdir(output):
-    makedirs(output)
+def clean_data(input_dir='f1db_csv/', output_dir='clean_f1db/'):
+    """For each file in input_dir, replace \\N with NaN and save"""
+    if not isdir(output_dir):
+        makedirs(output_dir)
 
-  # For each file, set column names and handle missing values
-  for filename in listdir('f1db_csv'):
-    df = pd.read_csv(join(input, filename))
-    df = df.replace(r'\N', np.NaN)
-    df.to_csv(join(output, filename), index=False)
+    # For each file, set column names and handle missing values
+    for filename in listdir(input_dir):
+        df = pd.read_csv(join(input_dir, filename))
+        df.replace(r'\N', np.NaN, inplace=True)
+        df.to_csv(join(output_dir, filename), index=False)
+
+    print("Cleaned data is now stored in " + output_dir)
 
 
 if __name__ == '__main__':
-  num_inputs = len(sys.argv)
-
-  if num_inputs == 1:
     clean_data()
-    print("Cleaned data is now stored in clean_f1db/")
-  elif num_inputs == 3:
-    clean_data(sys.argv[1], sys.argv[2])
-    print("Cleaned data is now stored in " + sys.argv[2] + '/')
-  else:
-    print("Expected either zero or two input arguments")
