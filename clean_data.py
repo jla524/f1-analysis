583ff1b465a36815545cca8ccfd96be14fbc9b7e
Jacky Lee <jla524@sfu.ca>
2021-09-09 20:06:35 -0700

    Use pathlib instead of os for file paths

--- clean_data.py
+++ clean_data.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
 from numpy import NaN
 from pandas import read_csv
-from os import listdir, makedirs
-from os.path import isdir, join
+from pathlib import Path
 
 
-INPUT_DIR = 'f1db_csv'
-OUTPUT_DIR = 'clean_f1db'
-
-
-def clean_data():
-    # Create output directory if it does not exist
-    if not isdir(OUTPUT_DIR):
-        makedirs(OUTPUT_DIR)
-
-    # For each file in INPUT_DIR, replace \N with NaN and save
-    for fn in listdir(INPUT_DIR):
-        df = read_csv(join(INPUT_DIR, fn))
+def clean_data(input_path=Path('f1db_csv'), output_path=Path('clean_f1db')):
+    output_path.mkdir(exist_ok=True)
+    # For each file in input_dir, replace \N with NaN and save
+    for file in input_path.iterdir():
+        df = read_csv(file)
         df.replace(r'\N', NaN, inplace=True)
-        df.to_csv(join(OUTPUT_DIR, fn), index=False)
-
-    print(f"Cleaned data is now stored in {OUTPUT_DIR}/")
+        df.to_csv(output_path / file.name, index=False)
+    print("Cleaned data is now stored in", output_path)
 
 
 if __name__ == '__main__':
